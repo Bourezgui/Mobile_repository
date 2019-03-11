7667bb72ef1cc124f7ee488988c8e002b3b2a478
khalil <bourezgui_khalil@outlook.com>
2019-03-11 23:37:55 +0100

    my message

--- Drocter/src/connexion/Urlconx.java
+++ Drocter/src/connexion/Urlconx.java
@@ -4,6 +4,6 @@ package connexion;
 public class Urlconx {
 	private static String[] ipadresse= {"127.0.0.1","192.168.1.51","172.16.10.20"};
 	//L'url utiliser pour connecter du webservice
-	public static final String url="http://"+ ipadresse[1] +":28880/adxwsvc/services/CAdxWebServiceXmlCC?wsdl";
+	public static final String url="http://"+ipadresse[1]+":28880/adxwsvc/services/CAdxWebServiceXmlCC?wsdl";
 
 }
